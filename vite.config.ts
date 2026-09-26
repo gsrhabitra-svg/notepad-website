2d28bb64a3bd454d660c3417a30b60cd09bae219
gsrhabitra-svg <gsrhabitra@gmail.com>
2026-09-26 07:32:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,29 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 
-    build: {
-      outDir: 'public',
-      emptyOutDir: true,
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
+  },
 
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
 
-    server: {
-      hmr:
-        process.env.DISABLE_HMR !== 'true',
-
-      watch:
-        process.env.DISABLE_HMR === 'true'
-          ? null
-          : {},
-    },
-  };
+    watch:
+      process.env.DISABLE_HMR === 'true'
+        ? null
+        : {},
+  },
 });
